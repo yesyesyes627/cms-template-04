62b258b4d87d5d9c825a6955055da4ffe1e56282
Oka li <okaoka0709@gmail.com>
2017-04-26 16:31:11 +0800

    更新 hud 錨點程式

--- Js/hud.js
+++ Js/hud.js
@@ -42,17 +42,15 @@ define(function(){
 		var $env = $('html,body'),
 			$a = $('a[href^="#"]').filter(function(i) {
 				var $this = $(this),
-					$closest = $this.closest('data-type');
+					$closest = $this.closest('[data-type]');
 
-				return ( $($this.attr('href')).length !== 0 && $closest.attr('class') !== 'list-text tab' );
+				return ( $($this.attr('href')).length !== 0 && !$closest.hasClass('list-text tab') );
 			});
 
 		$a.on(_eventNmae, function(){
 
 			var $this = $(this),
 				$href = $($this.attr('href'));
-
-			if( !$href.length ) { $href = $($set.bindNode) }
 
 			$env.stop().animate({
 				scrollTop : ( $href.offset().top ) - $in_h
