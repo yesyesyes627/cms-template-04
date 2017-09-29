b2624eeedec22f2e7e3dd98419720bc3f06f5e69
Oka li <okaoka0709@gmail.com>
2017-09-29 17:35:48 +0800

    1. 新增 7-zip 一鍵壓縮 2. 修正 langFilter 在本機測試環境下預設為中文

--- Js/langFilter.js
+++ Js/langFilter.js
@@ -1,7 +1,7 @@
 define(function(){ //取得網頁語系是否為中文，是的話回傳 true，不是的話回傳 false
 
     if( window.CCMS_IsChinese === undefined ) {
-        var _lang = window.CCMS_LanguageSN || $('body').data('lang');
+        var _lang = window.CCMS_LanguageSN || 1; //1 就是中文拉，預設中文
 
         if( _lang === 1 ) {
             window.CCMS_IsChinese = true;
---
+++ build.bat
@@ -0,0 +1,2 @@
+path C:\Program Files\7-Zip
+7z a -tzip %CD%.zip  Architecture.json Sass Js Images
